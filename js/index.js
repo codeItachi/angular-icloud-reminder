bf43e7c6bab3604dfa637077827ed246bf032efb
handsome <463517705@qq.com>
2016-03-19 19:25:06 +0800

    样式修改

--- js/index.js
+++ js/index.js
@@ -1,113 +1,86 @@
-var myapp=angular.module('reminder',[]);
-//过滤器
-myapp.filter('search',[function(){
-  return function(e,key){
-    var xx=function(items){
-      for(var i=0;i<items.length;i++){
-       if (items[i].title.indexOf(key)!=-1){
-        return true;
-       }
+var reminder = angular.module('myapp',[]);
+reminder.filter('search',[function(){
+	return function(data,key){
+	    var xx = function(items){
+	    	for(var i = 0;i<items.length;i++){
+	    		if(items[i].title.indexOf(key) != -1){
+                  return true;
+	    		}
+	    	}
+	    	return false;
+	    }
+      var r = [];
+      for(var i =0;i<data.length;i++){
+      	if(data[i].name.indexOf(key) != -1){
+      		r.push(data[i]);
+      	}
+      	if(xx(data[i].items)){
+      		r.push(data[i]);
+      	}
       }
-      return false;
-    }
-    var r=[];
-    for(var i=0;i<e.length;i++){
-      if(e[i].name.indexOf(key)!=-1 ){
-        r.push(e[i]);
-      }else if(xx(e[i].items)){
-        /*var re=[];
-        for(var j=0;j<e[i].length;j++){
-          if(){
-            re.push()
-          }
-        }
-        e[i].items= re;*/
-        r.push(e[i])
-      }
-    }
-    return r;
-  }
-}])
-
-//angular
-myapp.controller('rdctrl', ['$scope', function($scope){
-  var d=localStorage.data;
-  $scope.shijianliebiao=d?JSON.parse(d):[];
-  
-  $scope.colors=['purple','green','blue','yellow','brown','pink','orange'];
-  $scope.cindex=0;
-  //深拷贝
-  $scope.copy=angular.copy($scope.shijianliebiao)
-  $scope.countDone=function(){
-    var lis=$scope.shijianliebiao[$scope.cindex].items;
-    var r=0;
-    for(var i=0;i<lis.length;i++){
-      if(lis[i].isDone){
-        r+=1;
-      }
-    }
-    return r;
-  }
-  $scope.setItem=function(index){
-     $scope.cindex=index;
-     $scope.key=null;
-  }
-
-  $scope.addItem=function(){
-  	var data={
-  		name:'新列表'+($scope.shijianliebiao.length+1),
-  		color:$scope.colors[$scope.shijianliebiao.length%7],
-  		items:[]
-  	};
-  	$scope.shijianliebiao.push(data);
-  	localStorage.data=JSON.stringify($scope.shijianliebiao);
-  }
- 
-
-  //清空
-  $scope.delete=function(){
-  	var r=[];
-  	for(var i=0;i<$scope.shijianliebiao.length;i++){
-  		if(i!=$scope.cindex){
-  			r.push($scope.shijianliebiao[i])
-  		}
-  		
-  	}
-  	$scope.shijianliebiao=r;
-  	$scope.cindex=0;
-  	localStorage.data = JSON.stringify($scope.shijianliebiao);//保存数据
+      return r;
 	}
-  
-  
+}])
+reminder.controller('rdctrl', ['$scope', function($scope){
+	var d = localStorage.data;
+	$scope.shijianliebiao = d?JSON.parse(d):[];
 
-  //增加
-  $scope.addTodo=function(){
-  	var cu=$scope.shijianliebiao[$scope.cindex];
-  	var data={
-  		title:'',
-  		isDone:false
-  	}
-  	cu.items.push(data);
-  	localStorage.data=JSON.stringify($scope.shijianliebiao);
-  }
-  //删除
-  $scope.deleteTodo=function(index){
-  	var r=[];
-  	var cu=$scope.shijianliebiao[$scope.cindex];
-    for(var i=0;i<cu.items.length;i++){
-    	if(i!=index){
-    		r.push(cu.items[i])
-    	}
-    	
-    }
-    cu.items=r;
-    localStorage.data = JSON.stringify(cu.items);
-  }
- 
+	$scope.colors = ['purple','green','blue','yellow','brow','pink','orange'];
 
-  $scope.save=function(){
-  	localStorage.data = JSON.stringify($scope.shijianliebiao);
-  }
+	$scope.cindex = 0;
 
-  //改	
+	$scope.setItem = function(index) {
+		$scope.cindex = index;
+	}
+    $scope.save = function(){
+		localStorage.data = JSON.stringify($scope.shijianliebiao);
+	}
+	$scope.addItem = function() {
+		var data = {
+			name:'新列表 ' + ($scope.shijianliebiao.length+1),
+			color:$scope.colors[$scope.shijianliebiao.length%7],
+			items:[]
+		};
+		$scope.shijianliebiao.push(data);
+		localStorage.data = JSON.stringify($scope.shijianliebiao);
+	}
+
+	$scope.deleteItem = function() {
+		var r = [];
+		for(var i=0; i<$scope.shijianliebiao.length; i++){
+			if(i != $scope.cindex){
+				r.push($scope.shijianliebiao[i]);
+			}
+		}
+		$scope.shijianliebiao = r;
+		$scope.cindex = 0;
+		localStorage.data = JSON.stringify($scope.shijianliebiao);
+	}
+	$scope.countDone = function(){
+		var lis = $scope.shijianliebiao[$scope.cindex].items;
+		var r = 0;
+		for(var i = 0;i < lis.length;i++){
+			if(lis[i].isDone){
+				r += 1;
+			}
+		}
+		return r;
+	}
+	$scope.addTodo = function(){
+		var cu = $scope.shijianliebiao[$scope.cindex];
+		var data = {title:'新条目'+(cu.items.length+1),isDone:false}
+		cu.items.push(data)
+		localStorage.data = JSON.stringify($scope.shijianliebiao);
+	}
+	$scope.deleteTodo = function(index){
+        var r = [];
+        var cu = $scope.shijianliebiao[$scope.cindex];
+        for(var i = 0;i<cu.items.length;i++){
+           if(i != index){
+           	 r.push(cu.items[i])
+           }
+        }
+        cu.items = r;
+        localStorage.data = JSON.stringify($scope.shijianliebiao);
+	}
 }])
